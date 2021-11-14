6f8563726b14ea2f28ed9518e6ab42201bc3b968
quentinrouville <rouvilleq@gmail.com>
2021-11-14 18:52:01 +0100

    Add tests to test "isValid" method

--- src/test/java/fr/esgi/quentinrouville/moduleTests/VerifyApplicationTest.java
+++ src/test/java/fr/esgi/quentinrouville/moduleTests/VerifyApplicationTest.java
@@ -1,29 +1,40 @@
 package fr.esgi.quentinrouville.moduleTests;
 
+import fr.esgi.quentinrouville.common.domain.services.MemberService;
 import fr.esgi.quentinrouville.common.domain.services.RegisterService;
 import fr.esgi.quentinrouville.common.domain.services.VerifyMemberService;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class VerifyApplicationTest {
-    @Before
-    public void init() {
-        RegisterService registerService = new RegisterService();
+public class VerifyApplicationTest
+{
+    private final static RegisterService registerService = new RegisterService();
+    private final VerifyMemberService verifyMemberService = new VerifyMemberService();
+    private final MemberService memberService = new MemberService();
+
+    @BeforeClass
+    public static void init() {
         registerService.register("Quentin", "ROUVILLE", "rouvilleq@gmail.com", "password");
     }
 
     @Test
     public void ShouldReturnFalseWhenTheUserEmailIsntAlreadyOnTheStorage() {
-        VerifyMemberService verifyMemberService = new VerifyMemberService();
-
         assertFalse(verifyMemberService.emailIsAlreadyTaken("quentin.rouville@orange.fr"));
     }
     @Test
     public void ShouldReturnTrueWhenTheUserEmailIsAlreadyOnTheStorage() {
-        VerifyMemberService verifyMemberService = new VerifyMemberService();
-
         assertTrue(verifyMemberService.emailIsAlreadyTaken("rouvilleq@gmail.com"));
+    }
+
+    @Test
+    public void ShouldReturnFalseWhenTheUserIsNull() {
+        assertFalse(verifyMemberService.isValid(null));
+    }
+
+    @Test(expected = Exception.class)
+    public void ShouldReturnAnErrorWhenTheUserIsNull(){
+        memberService.create(null);
     }
 }
