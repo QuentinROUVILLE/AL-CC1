c8a6e8368467e5a9b957bf3f099b8e03cf04882a
quentinrouville <rouvilleq@gmail.com>
2021-11-14 17:45:55 +0100

    Add "isValid" method to VerifyMemberService

--- src/main/java/fr/esgi/quentinrouville/common/domain/services/VerifyMemberService.java
+++ src/main/java/fr/esgi/quentinrouville/common/domain/services/VerifyMemberService.java
@@ -1,6 +1,12 @@
 package fr.esgi.quentinrouville.common.domain.services;
 
+import fr.esgi.quentinrouville.common.domain.model.Member;
+
 public class VerifyMemberService {
+    public boolean isValid(Member member) {
+        return member != null && !emailIsAlreadyTaken(member.getEmail());
+    }
+
     public boolean emailIsAlreadyTaken(String email) {
         MemberService memberService = new MemberService();
         return memberService.findAll().stream()
