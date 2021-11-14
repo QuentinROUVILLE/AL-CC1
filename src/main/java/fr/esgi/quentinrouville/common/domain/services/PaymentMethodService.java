6cbc1dda0ca8fc4318d8021608eda01a8773f53a
quentinrouville <rouvilleq@gmail.com>
2021-11-14 21:05:29 +0100

    Add PaymentMethodService

---
+++ src/main/java/fr/esgi/quentinrouville/common/domain/services/PaymentMethodService.java
@@ -0,0 +1,17 @@
+package fr.esgi.quentinrouville.common.domain.services;
+
+import fr.esgi.quentinrouville.common.domain.model.Member;
+import fr.esgi.quentinrouville.common.domain.model.MemberId;
+
+public class PaymentMethodService
+{
+    private static final MemberService memberService = new MemberService();
+
+    public boolean ApprovePayment(MemberId memberId, double amount) {
+        Member member = memberService.findById(memberId);
+        if(member.getPaymentMethod() != null){
+            return member.getPaymentMethod().PayAmount(amount);
+        }
+        return false;
+    }
+}
