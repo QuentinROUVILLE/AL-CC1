5ea3ce790d1427ba295fe339aafa8e3f0bdb0155
quentinrouville <rouvilleq@gmail.com>
2021-11-14 16:06:41 +0100

    Rename "CreateAMember" test in ApplyMemberTest and add a test if not all values are provided

--- src/test/java/fr/esgi/quentinrouville/moduleTests/ApplyMemberTest.java
+++ src/test/java/fr/esgi/quentinrouville/moduleTests/ApplyMemberTest.java
@@ -9,11 +9,16 @@ import static org.junit.Assert.assertEquals;
 
 public class ApplyMemberTest {
     @Test
-    public void CreateAMember() {
+    public void ShouldCreateAMemberIfAllParametersAreProvided() {
         RegisterService registerService = new RegisterService();
         MemberService memberService = new MemberService();
         Member newMember = registerService.register("Quentin", "ROUVILLE", "rouvilleq@gmail.com", "password");
 
         assertEquals(memberService.byId(newMember.getMemberId()), newMember);
+    }
+    @Test(expected = NullPointerException.class)
+    public void ShouldReturnAErrorIfNotAllParametersAreProvided() {
+        RegisterService registerService = new RegisterService();
+        registerService.register("Quentin", "ROUVILLE", "rouvilleq@gmail.com", null);
     }
 }
