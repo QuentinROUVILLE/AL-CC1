84e58e62d7260c507829db2e6855deba888fd6d5
quentinrouville <rouvilleq@gmail.com>
2021-11-14 18:53:52 +0100

    Optimize ApplyMemberTest

--- src/test/java/fr/esgi/quentinrouville/moduleTests/ApplyMemberTest.java
+++ src/test/java/fr/esgi/quentinrouville/moduleTests/ApplyMemberTest.java
@@ -1,24 +1,25 @@
 package fr.esgi.quentinrouville.moduleTests;
 
-import fr.esgi.quentinrouville.common.domain.services.RegisterService;
 import fr.esgi.quentinrouville.common.domain.model.Member;
 import fr.esgi.quentinrouville.common.domain.services.MemberService;
+import fr.esgi.quentinrouville.common.domain.services.RegisterService;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ApplyMemberTest {
+    private final MemberService memberService = new MemberService();
+    private final RegisterService registerService = new RegisterService();
+
     @Test
     public void ShouldCreateAMemberIfAllParametersAreProvided() {
-        RegisterService registerService = new RegisterService();
-        MemberService memberService = new MemberService();
         Member newMember = registerService.register("Quentin", "ROUVILLE", "rouvilleq@gmail.com", "password");
 
         assertEquals(memberService.byId(newMember.getMemberId()), newMember);
     }
+
     @Test(expected = NullPointerException.class)
     public void ShouldReturnAErrorIfNotAllParametersAreProvided() {
-        RegisterService registerService = new RegisterService();
         registerService.register("Quentin", "ROUVILLE", "rouvilleq@gmail.com", null);
     }
 }
