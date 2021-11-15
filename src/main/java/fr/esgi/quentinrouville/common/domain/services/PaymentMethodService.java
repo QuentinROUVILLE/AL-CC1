5bb2e0c9a63b1a21a0009ef46bb59e2641f93338
quentinrouville <rouvilleq@gmail.com>
2021-11-15 18:26:20 +0100

    Add a method "ApproveTradesman" method

--- src/main/java/fr/esgi/quentinrouville/common/domain/services/PaymentMethodService.java
+++ src/main/java/fr/esgi/quentinrouville/common/domain/services/PaymentMethodService.java
@@ -6,11 +6,16 @@ import fr.esgi.quentinrouville.common.domain.model.MemberId;
 public class PaymentMethodService
 {
     private static final MemberService memberService = new MemberService();
+
     public boolean ProcessPayment(MemberId memberId, double amount) {
         Member member = memberService.findById(memberId);
         if(member.getPaymentMethod() != null){
             return member.getPaymentMethod().PayAmount(amount);
         }
         return false;
+    }
+
+    public void ApproveTradesman(){
+        System.out.println("Tradesman approve");
     }
 }
