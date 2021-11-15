eb46e86f65579098ac08f2a812b04b90f26976be
quentinrouville <rouvilleq@gmail.com>
2021-11-15 19:19:46 +0100

    Add a scenario in App

--- src/main/java/fr/esgi/quentinrouville/App.java
+++ src/main/java/fr/esgi/quentinrouville/App.java
@@ -1,9 +1,21 @@
 package fr.esgi.quentinrouville;
 
-public class App 
+import fr.esgi.quentinrouville.common.domain.model.Member;
+import fr.esgi.quentinrouville.common.domain.services.PaymentMethodService;
+import fr.esgi.quentinrouville.common.domain.services.RegisterService;
+import fr.esgi.quentinrouville.common.infrastructure.paymentMethod.CreditCardPayment;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(System.getProperty("java.version"));
+        RegisterService registerService = new RegisterService();
+        PaymentMethodService paymentMethodService = new PaymentMethodService();
+
+        Member me = registerService.register("Quentin", "ROUVILLE", "rouvilleq@gmail.com", "password");
+
+        me.setPaymentMethod(new CreditCardPayment("1234567890123456", "11/21", "123", "M QUENTIN ROUVILLE"));
+
+        paymentMethodService.ProcessPayment(me.getMemberId(), 49.99);
     }
 }
