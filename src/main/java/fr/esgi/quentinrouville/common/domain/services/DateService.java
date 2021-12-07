79c2376940e3a6d4b335cefdcbb049de81235bf1
quentinrouville <rouvilleq@gmail.com>
2021-12-07 16:00:54 +0100

    Add a method to get if a month and year is in the future

--- src/main/java/fr/esgi/quentinrouville/common/domain/services/DateService.java
+++ src/main/java/fr/esgi/quentinrouville/common/domain/services/DateService.java
@@ -1,6 +1,7 @@
 package fr.esgi.quentinrouville.common.domain.services;
 
 import java.time.LocalDate;
+import java.util.Calendar;
 
 public final class DateService
 {
@@ -14,5 +15,17 @@ public final class DateService
 
     public boolean isDateInThePast(LocalDate date){
         return date.isBefore(LocalDate.now());
+    }
+
+    public boolean isMonthAndYearInTheFuture(int month, int year){
+        year = reformateYear(year);
+        return isDateInTheFuture(LocalDate.of(year, month, 1));
+    }
+
+    private int reformateYear(int year){
+        if(year < 2000){
+            return year + (Calendar.getInstance().get(Calendar.YEAR) / 100) * 100;
+        }
+        return year;
     }
 }
--- src/test/java/fr/esgi/quentinrouville/services/DateServiceTests.java
+++ src/test/java/fr/esgi/quentinrouville/services/DateServiceTests.java
@@ -3,6 +3,7 @@ package fr.esgi.quentinrouville.services;
 import fr.esgi.quentinrouville.common.domain.services.DateService;
 import org.junit.Test;
 import java.time.LocalDate;
+import java.util.Calendar;
 
 import static org.junit.Assert.*;
 
@@ -30,5 +31,23 @@ public final class DateServiceTests {
     @Test
     public void testIsDateInThePastMethodWhenTheDateIsInTheFuture() {
         assertFalse(dateService.isDateInThePast(LocalDate.now().plusDays(1)));
+    }
+
+    @Test
+    public void testIsMonthAndYearInTheFutureWhenTheDateIsInTheFuture() {
+        assertTrue(dateService.isMonthAndYearInTheFuture(Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.YEAR) + 1));
+    }
+    @Test
+    public void testIsMonthAndYearInTheFutureWhenTheDateIsInThePast() {
+        assertFalse(dateService.isMonthAndYearInTheFuture(Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.YEAR) - 1));
+    }
+
+    @Test
+    public void testIsMonthAndYearInTheFutureWhenTheDateIsInTheFutureWithTwoDigitYear() {
+        assertTrue(dateService.isMonthAndYearInTheFuture(Calendar.getInstance().get(Calendar.MONTH), (Calendar.getInstance().get(Calendar.YEAR) + 1) % 100));
+    }
+    @Test
+    public void testIsMonthAndYearInTheFutureWhenTheDateIsInThePastWithTwoDigitYear() {
+        assertFalse(dateService.isMonthAndYearInTheFuture(Calendar.getInstance().get(Calendar.MONTH), (Calendar.getInstance().get(Calendar.YEAR) - 1) % 100));
     }
 }
