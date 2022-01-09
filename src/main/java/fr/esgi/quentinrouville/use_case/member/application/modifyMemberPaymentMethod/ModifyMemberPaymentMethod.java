316520fdf2ebc447729216f96f145ed239d77161
Quentin R <rouvilleq@gmail.com>
2022-01-09 19:58:14 +0100

    Fix ModifyMemberPaymentMethod

--- src/main/java/fr/esgi/quentinrouville/use_case/member/application/modifyMemberPaymentMethod/ModifyMemberPaymentMethod.java
+++ src/main/java/fr/esgi/quentinrouville/use_case/member/application/modifyMemberPaymentMethod/ModifyMemberPaymentMethod.java
@@ -1,20 +1,21 @@
 package fr.esgi.quentinrouville.use_case.member.application.modifyMemberPaymentMethod;
 
 import fr.esgi.quentinrouville.kernel.Command;
+import fr.esgi.quentinrouville.use_case.member.domain.MemberId;
 import fr.esgi.quentinrouville.use_case.member.exposition.PaymentMethodDTO;
 
 import java.util.Objects;
 
 public final class ModifyMemberPaymentMethod implements Command {
-    public final int memberId;
+    public final MemberId memberId;
     public final PaymentMethodDTO paymentMethod;
 
-    private ModifyMemberPaymentMethod(int memberId, PaymentMethodDTO paymentMethod) {
+    private ModifyMemberPaymentMethod(MemberId memberId, PaymentMethodDTO paymentMethod) {
         this.memberId = Objects.requireNonNull(memberId);
         this.paymentMethod = Objects.requireNonNull(paymentMethod);
     }
 
-    public static ModifyMemberPaymentMethod of(int memberId, PaymentMethodDTO paymentMethod) {
+    public static ModifyMemberPaymentMethod of(MemberId memberId, PaymentMethodDTO paymentMethod) {
         return new ModifyMemberPaymentMethod(memberId, paymentMethod);
     }
 }
