0fec77f3c9af88de710313319176e0959393d680
Quentin R <rouvilleq@gmail.com>
2022-01-09 19:21:11 +0100

    Fix VerifyApplicationTest

--- src/test/java/fr/esgi/quentinrouville/feature/VerifyApplicationTest.java
+++ src/test/java/fr/esgi/quentinrouville/feature/VerifyApplicationTest.java
@@ -1,6 +1,14 @@
 package fr.esgi.quentinrouville.feature;
 
+import fr.esgi.quentinrouville.kernel.EventDispatcher;
 import fr.esgi.quentinrouville.use_case.member.application.MemberService;
+import fr.esgi.quentinrouville.use_case.member.application.createMember.CreateMember;
+import fr.esgi.quentinrouville.use_case.member.application.createMember.CreateMemberCommandHandler;
+import fr.esgi.quentinrouville.use_case.member.domain.MemberRepository;
+import fr.esgi.quentinrouville.use_case.member.domain.MemberType;
+import fr.esgi.quentinrouville.use_case.member.exposition.EmailAddressDTO;
+import fr.esgi.quentinrouville.use_case.member.exposition.PasswordDTO;
+import fr.esgi.quentinrouville.use_case.member.infrastructure.InMemoryMemberRepository;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -8,14 +16,23 @@ import static org.junit.Assert.*;
 
 public final class VerifyApplicationTest
 {
-    private final static RegisterMember registerService = new RegisterMember();
+    private static final MemberRepository memberRepository = InMemoryMemberRepository.getInstance();
+    private static final EventDispatcher eventDispatcher = event -> System.out.println("Dispatching Event " + event.getClass().getName());
+
+    private static final CreateMemberCommandHandler createMemberCommandHandler = CreateMemberCommandHandler.of(memberRepository, eventDispatcher);
     private final VerifyMember verifyMember = new VerifyMember();
     private final MemberService memberService = new MemberService();
 
     @BeforeClass
     public static void init()
     {
-        registerService.register("Quentin", "ROUVILLE", "qrouville@gmail.com", "Azerty@123");
+        createMemberCommandHandler.handle(CreateMember.of(
+            MemberType.CONTRACTOR,
+            "Quentin",
+            "ROUVILLE",
+            EmailAddressDTO.of("qrouville@myges.fr"),
+            PasswordDTO.of("Azerty123@")
+    ));
     }
 
     @Test
@@ -27,7 +44,7 @@ public final class VerifyApplicationTest
     @Test
     public void ShouldReturnTrueWhenTheUserEmailIsAlreadyOnTheStorage()
     {
-        assertTrue(verifyMember.emailIsAlreadyTaken("qrouville@gmail.com"));
+        assertTrue(verifyMember.emailIsAlreadyTaken("qrouville@myges.fr"));
     }
 
     @Test
